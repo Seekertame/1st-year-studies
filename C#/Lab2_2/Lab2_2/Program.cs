e254d70e724d6d51100a162ae5a487366153fe76
Seekertame <118879602+Seekertame@users.noreply.github.com>
2024-11-17 18:50:31 +0300

    Add files via upload
    HSE

---
+++ C#/Lab2_2/Lab2_2/Program.cs
@@ -0,0 +1,106 @@
+﻿// Бюрчиев Тимур Зольванович, БПИ 248-1, вариант 11
+// https://disk.yandex.ru/d/tu2XoYE5redafA
+using System;
+using System.Text;
+namespace Lab2_2
+{
+    /// <summary>
+    /// Содержит методы для решения задачи.
+    /// </summary>
+    public class CharJaggedArray
+    {
+        /// <summary>
+        /// _arr - ссылка на массив символьных массивов
+        /// </summary>
+        private char[][] _arr;
+        
+        /// <summary>
+        /// Связывает _arr с массивом символьных массивов, состоящим из 5 символьных массивов
+        /// </summary>
+        private void CreateCj()
+        {
+            _arr = new char[5][];
+            for (int i = 0; i < 5; i++)
+            {
+                _arr[i] = new char[5];
+                for (int j = 0; j < 5; j++)
+                {
+                    _arr[i][j] = i + j >= 4 ? 'X' : 'O';
+                }
+            }
+        }
+        /// <summary>
+        /// Связывает _arr с массивом символьных массивов, состоящим из N символьных массивов
+        /// </summary>
+        /// <param name="n">Размерность массива</param>
+        private void CreateCj(int n)
+        {
+            _arr = new char[n][];
+            for (int i = 0; i < n; i++)
+            {
+                _arr[i] = new char[n];
+                for (int j = 0; j < n; j++)
+                {
+                    _arr[i][j] = i + j >= n - 1 ? 'X' : 'O';
+                }
+            }
+        }
+        /// <summary>
+        /// Возвращает строку с отформатированным представлением структуры данных arr
+        /// </summary>
+        /// <returns>Строка с отформатированным представлением структуры данных arr</returns>
+        public string ArrayToString()
+        {
+            if (_arr == null)
+            {
+                return string.Empty;
+            }
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < _arr.Length; i++)
+            {
+                sb.Append(string.Join(" ", _arr[i]));
+                if (i != _arr.Length - 1)
+                {
+                    sb.AppendLine();
+                }
+            }
+            return sb.ToString().Trim();
+        }
+        /// <summary>
+        /// Конструктов без параметров
+        /// </summary>
+        public CharJaggedArray()
+        {
+            CreateCj();
+        }
+        /// <summary>
+        /// Конструктор с параметром
+        /// </summary>
+        /// <param name="n">Размерность массива.</param>
+        public CharJaggedArray(int n)
+        {
+            if (n > 0 && n < 17)
+            {
+                CreateCj(n);
+            }
+            else
+            {
+                CreateCj();
+            }
+        }
+    }
+    public class Program
+    {
+        public static void Main()
+        {
+            CharJaggedArray array5 = new CharJaggedArray();
+            Console.WriteLine(array5.ArrayToString());
+
+            CharJaggedArray array7 = new CharJaggedArray(16);
+            Console.WriteLine(array7.ArrayToString());
+
+            CharJaggedArray arrayInvalid = new CharJaggedArray(17);
+            Console.WriteLine(arrayInvalid.ArrayToString());
+        }
+    }
+}
---
+++ C#/Self2_1/Self2_1/Program.cs
@@ -0,0 +1,131 @@
+﻿using System.Text;
+namespace Self2_1
+{
+    public class Circle
+    {
+        private double _r;
+
+        public double Radius
+        {
+            get { return _r; }
+            set { _r = value > 0 ? value : throw new ArgumentException("Радиус должен быть положительным."); }
+        }
+
+        public double S
+        {
+            get { return Math.PI * _r * _r; }
+        }
+
+        public Circle()
+        {
+            _r = 1;
+        }
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+    }
+
+    class Rectangle
+    {
+        private double height;
+        private double width;
+
+        public double Height
+        {
+            get { return height; }
+            set { height = value > 0 ? value : 1; }
+        }
+
+        public double Width
+        {
+            get { return width; }
+            set { width = value > 0 ? value : 1; }
+        }
+
+        public double Perimeter
+        {
+            get { return 2 * (width + height); }
+        }
+
+        public double Area
+        {
+            get { return width * height; }
+        }
+
+        public override string ToString()
+        {
+            return $"Rectangle: Height = {height}, Width = {width}, Perimeter = {Perimeter}, Area = {Area}";
+        }
+
+        public Rectangle()
+        {
+            height = 1;
+            width = 1;
+        }
+
+        public Rectangle(double height, double width)
+        {
+            this.height = height;
+            this.width = width;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("Введите высоту первого прямоугольника:");
+            double height1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите ширину первого прямоугольника:");
+            double width1 = Convert.ToDouble(Console.ReadLine());
+
+            Rectangle rect1 = new Rectangle(height1, width1);
+
+            Console.WriteLine("Введите высоту второго прямоугольника:");
+            double height2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите ширину второго прямоугольника:");
+            double width2 = Convert.ToDouble(Console.ReadLine());
+
+            Rectangle rect2 = new Rectangle(height2, width2);
+
+            Console.WriteLine("\nПервый прямоугольник:");
+            Console.WriteLine($"Площадь: {rect1.Area}");
+            Console.WriteLine($"Периметр: {rect1.Perimeter}");
+
+            Console.WriteLine("\nВторой прямоугольник:");
+            Console.WriteLine($"Площадь: {rect2.Area}");
+            Console.WriteLine($"Периметр: {rect2.Perimeter}");
+
+
+
+            Console.WriteLine("Введите минимальное значение радиуса (Rmin):");
+            double Rmin = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите максимальное значение радиуса (Rmax):");
+            double Rmax = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите величину шага (delta):");
+            double delta = Convert.ToDouble(Console.ReadLine());
+
+            if (Rmin < 0 || Rmax <= Rmin || delta <= 0)
+            {
+                Console.WriteLine(
+                    "Некорректные значения диапазона или шага. Убедитесь, что Rmin >= 0, Rmax > Rmin и delta > 0.");
+                return;
+            }
+
+            Circle circle = new Circle();
+
+            Console.WriteLine("\nЗначения площади круга для различных значений радиуса:");
+
+            for (double r = Rmin; r <= Rmax; r += delta)
+            {
+                circle.Radius = r;
+                Console.WriteLine($"Радиус: {r:F2}, Площадь: {circle.S:F2}");
+            }
+        }
+    }
+}
